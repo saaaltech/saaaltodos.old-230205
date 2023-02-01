d94c5f3944a1d777391b81ea32996be2f5ba425e
hillzwei <saaaltech@163.com>
2023-02-01 23:09:10 +0800

    feat: validation of app root locale

--- lib/validate/app_root_validate.dart
+++ lib/validate/app_root_validate.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:saaaltodos/status/app_root.dart' as raw show appRoot;
 import 'package:saaaltodos/status/app_root.dart' hide appRoot;
+import 'package:saaaltodos/tools/widgets_helper.dart';
 
 /// Validate theme mode of the [AppRoot].
 ///
@@ -25,9 +26,18 @@ class ThemeModeValidate extends StatelessWidget {
     final controller = Row(
       mainAxisSize: MainAxisSize.min,
       children: [
-        TextButton(onPressed: () => toDark(), child: const Text('to dark')),
-        TextButton(onPressed: () => toLight(), child: const Text('to light')),
-        TextButton(onPressed: () => toSystem(), child: const Text('to system')),
+        TextButton(
+          onPressed: () => toDark(key: appRoot),
+          child: const Text('to dark'),
+        ),
+        TextButton(
+          onPressed: () => toLight(key: appRoot),
+          child: const Text('to light'),
+        ),
+        TextButton(
+          onPressed: () => toSystem(key: appRoot),
+          child: const Text('to system'),
+        ),
       ],
     );
 
@@ -37,6 +47,37 @@ class ThemeModeValidate extends StatelessWidget {
         Text('brightness: ${Theme.of(context).brightness}'),
         Text('platform: ${MediaQuery.of(context).platformBrightness}'),
         controller,
+      ],
+    );
+  }
+}
+
+class LocaleValidate extends StatelessWidget {
+  LocaleValidate({Key? key, GlobalKey? appRoot}) : super(key: key) {
+    this.appRoot = appRoot ?? raw.appRoot;
+  }
+
+  late final GlobalKey appRoot;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text('current locale: ${locale(context).theLanguageName}'),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextButton(
+              onPressed: () => setLocale(const Locale('en'), key: appRoot),
+              child: const Text('to en'),
+            ),
+            TextButton(
+              onPressed: () => setLocale(const Locale('zh'), key: appRoot),
+              child: const Text('to zh'),
+            ),
+          ],
+        ),
       ],
     );
   }
