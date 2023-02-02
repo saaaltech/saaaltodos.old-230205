d43b43affe0b5ae916105e92ef3b75ed85f817d5
hillzwei <saaaltech@163.com>
2023-02-02 19:46:37 +0800

    reco: add some docs and mark terminal background color

--- lib/components/terminal.dart
+++ lib/components/terminal.dart
@@ -7,11 +7,12 @@ import 'package:saaaltodos/tools/logger.dart';
 /// There's usually only one [TerminalContainer] widget in the whole app,
 /// as it is recommended.
 ///
-final terminalContainer = GlobalKey();
+final terminalContainer = GlobalKey(debugLabel: 'terminal container');
 
 /// Tab shortcuts to show or hide terminal pad.
 ///
 /// It is designed for desktop and will not be compatible with phones.
+/// And it is recommended to be used only in the app root.
 ///
 class TerminalContainer extends StatefulWidget {
   TerminalContainer({
@@ -24,10 +25,10 @@ class TerminalContainer extends StatefulWidget {
   final Widget mainArea;
 
   @override
-  State<TerminalContainer> createState() => _TerminalContainerState();
+  State<TerminalContainer> createState() => TerminalContainerState();
 }
 
-class _TerminalContainerState extends State<TerminalContainer> {
+class TerminalContainerState extends State<TerminalContainer> {
   // Controller of show or hide terminal pad.
   bool _show = false;
   bool get show => _show;
@@ -48,7 +49,10 @@ class _TerminalContainerState extends State<TerminalContainer> {
   /// Terminal pad display.
   Widget get terminal {
     return Positioned(
-      child: widget.terminalPad,
+      child: Container(
+        color: Colors.teal,
+        child: widget.terminalPad,
+      ),
     );
   }
 
