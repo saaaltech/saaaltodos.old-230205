1f53cdfc5839d8b578c67a07aeadd478ddc9482a
hillzwei <saaaltech@163.com>
2023-02-02 22:18:10 +0800

    reco: shortcut json convert api design

---
+++ lib/components/shortcut.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+// todo create a widget to select shortcut by pressing the shortcut.
+
+SingleActivator? resolveSingleActivator(dynamic raw) {
+  return null;
+}
+
+extension SingleActivatorJsonApis on SingleActivator {
+  String get code {
+    return '';
+  }
+}
+
+LogicalKeyboardKey? resolveKey(dynamic raw) {
+  return null;
+}
